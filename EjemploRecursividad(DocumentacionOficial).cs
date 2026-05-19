96ca424da89833bbc85eae00b3e23d3263d03438
Nacho <ignacio.traverso@bue.edu.ar>
2026-05-19 11:44:51 -0300

    Add files via upload

---
+++ EjemploRecursividad(DocumentacionOficial).cs
@@ -0,0 +1,21 @@
+﻿
+int fact, num;
+Console.Write("Enter a number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+ // calling recursive function   
+fact = factorial(num);
+
+Console.WriteLine("Factorial of {0} is {1}", num, fact);
+
+
+int factorial(int num)
+{
+    // termination condition
+    if (num == 0)
+        return 1;
+    else
+        // recursive call
+        return num * factorial(num - 1);
+}
+        
