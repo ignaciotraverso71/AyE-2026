a4f80b431c8ad3966b8f1fed0cfba7b27bfba6da
Nacho <ignacio.traverso@bue.edu.ar>
2026-09-15 11:02:21 -0300

    Add files via upload

---
+++ Arbol/Arbol.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiArbolito2
+{
+    public class Arbol
+    {
+        public Nodo raiz { get; set; }
+
+        public Arbol() {
+            raiz = null;
+        }
+
+        public void insertar(int valor) {
+            raiz = insertarRecursivo(raiz, valor);
+        }
+
+        public Nodo insertarRecursivo(Nodo nodoActual, int valor) {
+            if (nodoActual == null) {
+                return new Nodo(valor);
+            }
+            if (valor < nodoActual.valor) {
+                nodoActual.izquierdo = insertarRecursivo(nodoActual.izquierdo, valor);
+            } else if (valor > nodoActual.valor) {
+                nodoActual.derecho = insertarRecursivo(nodoActual.derecho, valor);
+            }
+            
+            return nodoActual;
+        }
+
+        public bool buscar(int valor) {
+            return buscarRecursivo(raiz, valor);
+        }
+
+        public bool buscarRecursivo(Nodo nodoActual, int valor) {
+            if (nodoActual == null) {
+                return false;
+            }
+            if (nodoActual.valor == valor)
+            {
+                return true;
+            }
+            
+            if (valor < nodoActual.valor) {
+                return buscarRecursivo(nodoActual.izquierdo, valor);
+            }
+            else {
+                return buscarRecursivo(nodoActual.derecho, valor);
+            }
+
+        }
+
+
+
+    }
+}
---
+++ Arbol/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MiArbolito2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Arbol arbolitox = new Arbol();
+
+            arbolitox.insertar(50);
+            arbolitox.insertar(20);
+            arbolitox.insertar(30);
+            arbolitox.insertar(70);
+            arbolitox.insertar(10);
+            arbolitox.insertar(80);
+
+            Console.WriteLine("Busqueda valor");
+            Console.WriteLine(arbolitox.buscar(15));
+
+        }
+    }
+}
